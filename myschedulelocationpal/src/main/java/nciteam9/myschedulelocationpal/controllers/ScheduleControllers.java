41e56071eacd7d9d5d53e00b65f7cd19e19051b9
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2026-03-04 18:01:21 +0000

    Get request to retrieve all the scheduled events for a specific user

--- myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/controllers/ScheduleControllers.java
+++ myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/controllers/ScheduleControllers.java
@@ -5,8 +5,11 @@ import nciteam9.myschedulelocationpal.repositories.ScheduleRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/schedule")
 public class ScheduleControllers {
 
     private final ScheduleRepository scheduleRepository;
@@ -15,10 +18,23 @@ public class ScheduleControllers {
         this.scheduleRepository = scheduleRepository;
     }
 
-    // Sets up the RESTFUL POST Method
-    @PostMapping("/schedule")
-    public ResponseEntity<Schedule> getSchedule(@RequestBody Schedule schedule) {
+    /*
+    Sets up the RESTFUL POST Method to store a schedule
+     */
+    @PostMapping
+    public ResponseEntity<Schedule> setSchedule(@RequestBody Schedule schedule) {
         Schedule save = scheduleRepository.save(schedule);
         return ResponseEntity.ok(save);
+    }
+
+    /*
+    Sets up the RESTFUL GET Method to retrieve all the scheduled events for a specific user
+    */
+    @GetMapping
+    public ResponseEntity<List<Schedule>> getSchedule(
+            @RequestParam int userId
+    ) {
+        List<Schedule> requestSchedules = scheduleRepository.findByuserId(userId);
+        return ResponseEntity.ok(requestSchedules);
     }
 }
--- myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/repositories/ScheduleRepository.java
+++ myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/repositories/ScheduleRepository.java
@@ -3,6 +3,10 @@ package nciteam9.myschedulelocationpal.repositories;
 import nciteam9.myschedulelocationpal.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+
+    List<Schedule> findByuserId(Integer userId);
 }
