36d11711dad5499e285644158b474ae78378269c
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2026-03-05 17:20:27 +0000

    Email Sender - first prototype the email notification. SendEmail class fully working. Still need to add a timer and to get the user's data

--- myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/MyschedulelocationpalApplication.java
+++ myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/MyschedulelocationpalApplication.java
@@ -1,13 +1,27 @@
 package nciteam9.myschedulelocationpal;
 
+import nciteam9.myschedulelocationpal.service.SendEmail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MyschedulelocationpalApplication {
+    private final SendEmail se;
+
+    public MyschedulelocationpalApplication(SendEmail se) {
+        this.se = se;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(MyschedulelocationpalApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void send() {
+        se.sendMessage("foudyantoine@gmail.com", "test", "Hello World");
     }
 
 }
