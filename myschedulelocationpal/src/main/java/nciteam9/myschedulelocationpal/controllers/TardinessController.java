2259364c3e46fd4cdafee5d91ad8614477d50996
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2026-03-28 19:16:02 +0000

    Updated email notifications to give the address instead of the coordinates
    Added TardinessController to request the user's record on how many times they have been late and on time

---
+++ myschedulelocationpal/src/main/java/nciteam9/myschedulelocationpal/controllers/TardinessController.java
@@ -0,0 +1,34 @@
+package nciteam9.myschedulelocationpal.controllers;
+
+import nciteam9.myschedulelocationpal.entities.User;
+import nciteam9.myschedulelocationpal.repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/tardiness")
+public class TardinessController {
+
+    private final UserRepository userRepository;
+
+    public TardinessController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity getTardiness(@RequestParam Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));;
+        ArrayList<String> userRecord = new ArrayList<>();
+        String onTime = String.valueOf(user.getOnTime());
+        String late = String.valueOf(user.getLate());
+        userRecord.add(onTime);
+        userRecord.add(late);
+
+        return ResponseEntity.ok(userRecord);
+    }
+}
